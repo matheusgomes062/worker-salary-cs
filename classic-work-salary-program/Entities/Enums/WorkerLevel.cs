2bba54708ef1477eaea29134074e321256205463
Matheus Gomes <matheusgomes062@gmail.com>
2020-01-05 18:11:11 -0300

    first commit but the program is done

---
+++ classic-work-salary-program/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classic_work_salary_program.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
